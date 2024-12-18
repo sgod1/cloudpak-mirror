596eaf78a8f4ba17d8d6d1d6f77c80dda84eb1ed
sgod1 <simon@godik.com>
2024-12-18 10:52:39 -0800

    Update 4-mirror-images.sh
    comment update

--- 4-mirror-images.sh
+++ 4-mirror-images.sh
@@ -7,7 +7,4 @@ export PATH=./bin:$PATH
 
 # you can pass --insecure flag to this command
 
-# pass --filter option to mirror subset of images
-# filter for baw: --filter ibmcp4baProd,ibmcp4baBASImages,ibmcp4baAAEImages,ibmEdbStandard
-
 oc image mirror -f $IBMPAK_HOME/.ibm-pak/data/mirror/$CASE_NAME/$CASE_VERSION/images-mapping.txt --filter-by-os '.*' -a $REGISTRY_AUTH_FILE --skip-multiple-scopes --max-per-registry=1
