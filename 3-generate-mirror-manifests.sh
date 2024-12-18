d1534218c9f155238da49dbe9830eee07eb98ab8
sgod1 <simon@godik.com>
2024-12-18 10:11:25 -0800

    Update 3-generate-mirror-manifests.sh
    add filters

--- 3-generate-mirror-manifests.sh
+++ 3-generate-mirror-manifests.sh
@@ -5,5 +5,8 @@ source ./registry.env
 
 export PATH=./bin:$PATH
 
-oc ibm-pak generate mirror-manifests $CASE_NAME $TARGET_REGISTRY --version $CASE_VERSION
+export baw_filter="ibmcp4baProd,ibmcp4baBAWImages,ibmcp4baFNCMImages,ibmcp4baBANImages,ibmcp4baBASImages,ibmcp4baAAEImages,ibmEdbStandard,ibmcp4baUMSImages,ibmcp4baPFSImages"
 
+export wfps_filter="ibmcp4baProd,ibmcp4baAAEImages,ibmcp4baBASImages,ibmcp4baWFPSImages,ibmEdbStandard"
+
+oc ibm-pak generate mirror-manifests $CASE_NAME $TARGET_REGISTRY --version $CASE_VERSION --filter $baw_filter
