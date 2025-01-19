768bb14c4b0731d106bd2059c77f19df093d02e4
Simon Godik <simon@godik.com>
2025-01-19 03:27:40 +0000

    filter variable

--- 3-generate-mirror-manifests.sh
+++ 3-generate-mirror-manifests.sh
@@ -5,5 +5,15 @@ source ./registry.env
 
 export PATH=./bin:$PATH
 
+oc ibm-pak config mirror-tools --enabled $MIRROR_TOOLS
+
+# define filter variable in the case.env file
+export baw_filter="ibmcp4baProd,ibmcp4baBAWImages,ibmcp4baFNCMImages,ibmcp4baBANImages,ibmcp4baBASImages,ibmcp4baAAEImages,ibmEdbStandard,ibmcp4baUMSImages,ibmcp4baPFSImages"
+export wfps_filter="ibmcp4baProd,ibmcp4baAAEImages,ibmcp4baBASImages,ibmcp4baWFPSImages,ibmEdbStandard"
+
+if test ! -z "$filter"; then
+oc ibm-pak generate mirror-manifests $CASE_NAME $TARGET_REGISTRY --version $CASE_VERSION --filter $filter
+else
 oc ibm-pak generate mirror-manifests $CASE_NAME $TARGET_REGISTRY --version $CASE_VERSION
+fi
 
