14c761bf0154062a674f05ea76cd038d1528a7b4
Simon Godik <simon@godik.com>
2025-02-03 20:46:32 +0000

    fixed typos

--- 4-mirror-oc-mirror.sh
+++ 4-mirror-oc-mirror.sh
@@ -4,15 +4,21 @@ source ${1:-"./case.env"}
 source "./registry.env"
 
 #
+# invocation: mirror-oc-mirror.sh [case.env ["tofile"|"fromfile"] ]
+#
+
+#
 # second parameter is "tofile" or "fromfile"
 #
-if test "$2"="tofile"; then
-file="file://${IBMPAK_HOME}"
+if test ! -z "$2"; then
+   if test "$2" = "tofile"; then
+      file="file://${IBMPAK_HOME}"
 
-elif test "$2"="fromfile"; then
-seqfile=${3:-"mirror_seq1_000000.tar"}
-fromfile=${IBMPAK_HOME}/${seqfile}
-fi
+   elif test "$2" = "fromfile"; then
+      seqfile=${3:-"mirror_seq1_000000.tar"}
+      fromfile=${IBMPAK_HOME}/${seqfile}
+   fi
+fi # ! -z $2
 
 VER=$CASE_LATEST_VERSION
 if test -z $VER; then
@@ -26,17 +32,23 @@ oc ibm-pak config mirror-tools --enabled $MIRROR_TOOLS
 if test ! -z $fromfile; then
 
    if test -f $fromfile; then
-   oc mirror --dest-skip-tls --from=${fromfile} docker:${TARGET_REGISTRY}
+      oc mirror --dest-skip-tls --from=${fromfile} docker:${TARGET_REGISTRY}
 
    else
-   echo mirror file $fromfile not found...
-   exit 1
+      echo mirror file $fromfile not found...
+      exit 1
    fi
 
 else
-target=${file:-$TARGET_REGISTRY}
+   target=${file:-"docker://$TARGET_REGISTRY"}
 
-# you can pass --insecure flag to this command
-oc mirror --config $IBMPAK_HOME/.ibm-pak/data/mirror/$CASE_NAME/$CASE_VERSION/image-set-config.yaml $target --dest-skip-tls --max-per-registry=6
+   if test ! -z "$file"; then
+      echo mirror from the mothership to directory $file
+   else
+      echo mirror from the mothership to private docker registry $TARGET_REGISTRY
+   fi
+
+   # you can pass --insecure flag to this command
+   oc mirror --config $IBMPAK_HOME/.ibm-pak/data/mirror/$CASE_NAME/$CASE_VERSION/image-set-config.yaml $target --dest-skip-tls --max-per-registry=6
 fi
 
