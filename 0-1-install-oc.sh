83491962b362f7dd1a1608e03b9c2588147851f6
Simon Godik <simon@godik.com>
2025-02-03 21:06:37 +0000

    check for rhel versions

--- 0-1-install-oc.sh
+++ 0-1-install-oc.sh
@@ -5,18 +5,35 @@
 os=`uname -o`
 arch=`uname -m`
 
-if test $os == "GNU/Linux" && test $arch == "x86_64"; then
-   octargz="openshift-client-linux.tar.gz"
+if test $os = "GNU/Linux" -a $arch = "x86_64"; then
 
-elif test $os == "Darwin" && test $arch == "x86_64"; then
+   if test -f /etc/redhat-release; then
+      rhel=`cat /etc/redhat-release | cut -d" " -f 4 | cut -d"." -f 1`
+
+      if test $rhel = "8"; then
+	 # rhel 8
+         echo linux rhel8, x86_64
+         octargz="openshift-client-linux-amd64-rhel8.tar.gz"
+      else
+	 # rhel 9
+         echo linux rhel9, x86_64
+         octargz="openshift-client-linux.tar.gz"
+      fi
+
+   else
+      # linux other than rhel
+      echo linux other than rhel, x86_64
+      octargz="openshift-client-linux.tar.gz"
+   fi
+
+elif test $os = "Darwin" -a $arch = "x86_64"; then
+   echo darwin, x86_64
    octargz="openshift-client-mac.tar.gz"
 
 else
    echo update this script to check for $os and $arch combination
    exit 1
 fi
-
-#octargz="openshift-client-linux-amd64-rhel8.tar.gz"
 
 if test -f ./$octargz; then
    rm ./$octargz
