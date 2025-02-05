9123bc57fa33715b0ae604cd7a650982a4b259c7
Simon Godik <simon@godik.com>
2025-02-05 02:23:25 +0000

    pass more args to 2-download-case-files.sh

--- 2-download-case-files.sh
+++ 2-download-case-files.sh
@@ -3,6 +3,8 @@
 source ${1:-"./case.env"}
 
 cpak=${2:-"cpak"}
+# pass an arg: --skip-dependencies
+cpakarg=$2
 
 export PATH=./bin:$PATH
 
@@ -11,5 +13,5 @@ oc ibm-pak config mirror-tools --enabled $MIRROR_TOOLS
 if test "$cpak" = "cp4ba" ; then
 oc ibm-pak get -c file://${IBMPAK_HOME}/cert-kubernetes/scripts/airgap/cp4ba-case-to-be-mirrored-${CASE_BRANCH}.txt
 else
-oc ibm-pak get ${CASE_NAME} --version ${CASE_VERSION}
+oc ibm-pak get ${CASE_NAME} --version ${CASE_VERSION} ${cpakarg}
 fi
